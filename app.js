850d266c4edeb03f4850079c6837f0bb90f2572f
Yash Narkhedkar <87686946+yashnarkhedkar@users.noreply.github.com>
2023-04-17 09:20:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 const express = require("express");
 const mongoose = require('mongoose')
 const Router = require("./routes/index.js")
-const bodyParser = require('body-parser');
-const session = require('express-session')
 const passport = require('passport')
-const LocalStrategy = require('passport-local').Strategy;
-const User = require('./schema/user.js')
-const bcrypt = require('bcryptjs')
 const cors = require('cors');
-const jwt = require('jsonwebtoken');
+const { intializePassport, isAuthenticated } = require('./passportConfig.js');
+const expressSession = require('express-session')
 
 require('dotenv').config();
 const app = express();
@@ -21,58 +17,22 @@ db.on("error", console.error.bind(console, "connection error: "));
 db.once("open", function () {
     console.log('Connected');
 });
-
 app.use(Router)
-// Middleware
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(session({ secret: 'cat', resave: false, saveUninitialized: false }));
-app.use(passport.initialize());
+intializePassport(passport);
+app.use(express.json())
+app.use(express.urlencoded({extended : true}));
+app.use(expressSession({secret : 'secret', resave : false, saveUninitialized : false}))
+app.use(passport.initialize())
 app.use(passport.session());
-
-// Define the serializeUser function
-passport.serializeUser((user, done) => {
-    done(null, user.id);
-  });
-  
-  // Define the deserializeUser function
-passport.deserializeUser((id, done) => {
-    User.findById(id)
-      .then(user => {
-        done(null, user);
-      })
-      .catch(err => {
-        done(err);
-      });
-  });
 
 
 app.post('/login', passport.authenticate('local'), (req, res) => {
-    const token = jwt.sign({ id: req.user.id, email : req.body.email }, 'cat');
     res.send({ message: 'Login successful.', token });
 });
 
-passport.use(new LocalStrategy({
-    usernameField: 'email',
-    passwordField: 'password',
-}, (email, password, done) => {
-    try {    
-        User.findOne({ email: email })
-            .then(user => {
-                if (user === null) {
-                    return done(null, false, { message: 'Incorrect email.' });
-                } 
-                bcrypt.compare(password, user.password, (err, res) => {
-                    if (res) {
-                        return done(null, user);
-                    } else {
-                        return done(null, false, { message: 'Incorrect password.' });
-                    }
-                });
-            })
-    } catch (error) {
-        return done(null);
-    }
-}));
+
+app.get('/profile', isAuthenticated, (req, res) => {
+    res.send(req.user)
+})
 
 app.listen(PORT, console.log(`Server started at ${PORT}`));
